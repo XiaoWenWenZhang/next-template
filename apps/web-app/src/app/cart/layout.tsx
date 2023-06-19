7891508225320e02c8fe33f07470bc47c96731d3
lan.jiang <lan.jiang@thoughtworks.com>
2023-06-19 18:07:25 +0800

    add cart layout

---
+++ apps/web-app/src/app/cart/layout.tsx
@@ -0,0 +1,11 @@
+import Head from "src/app/head";
+export default function Root({children}: { children: React.ReactNode }) {
+    return (
+        <html>
+        <body>
+        <Head/>
+        {children}
+        </body>
+        </html>
+    );
+}
