0f5cf149bb1139a936373f982b5e12244741f7f2
lan.jiang <lan.jiang@thoughtworks.com>
2023-06-19 14:08:18 +0800

    remove unused variable

--- apps/web-app/src/app/cart/page.tsx
+++ apps/web-app/src/app/cart/page.tsx
@@ -1,21 +1,22 @@
 import {ICart} from "src/types/cart";
 
-export default async function Page({params}: { params: { slug: string } }) {
+export default async function Page() {
     const cartResponse = await fetch(`https://twworkspace--vtexsgdemostore.myvtex.com/_v/cartPage/cd931e6fe5224a93b2864b7e7361ca1d`);
     const product: ICart = await cartResponse.json();
     const cartProductSkus = product.items;
 
     return (
-        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-            {cartProductSkus.map(productSku => (
-                <div key={productSku.id} style={{flex: '1', alignSelf: 'flex-start'}}>
-                    <img src={productSku.imageUrl}
-                         alt={productSku.name} width="100" height="100"/>
-                    <div>
-                        {productSku.name}
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center',
+            alignItems: 'center', borderBottom: '1px solid #ccc', width: '50vw', marginLeft: '20px'}}>
+            {cartProductSkus.map(cartItem => (
+                <div key={cartItem.id} style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                    <img src={cartItem.imageUrl}
+                         alt={cartItem.name} width="100" height="100"/>
+                    <div style={{margin: '0 20px'}}>
+                        {cartItem.name}
                     </div>
-                    <div>
-                        {productSku.price}
+                    <div style={{fontWeight: 500}}>
+                        R${cartItem.price}
                     </div>
                 </div>
             ))}
