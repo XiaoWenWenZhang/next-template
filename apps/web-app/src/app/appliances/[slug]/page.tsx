e78dde22de29ec4a4c03ec572d1079d041fb9f99
wen <wen.xiao@thoughtworks.com>
2023-06-27 11:14:38 +0800

    change the productList structure

--- apps/web-app/src/app/appliances/[slug]/page.tsx
+++ apps/web-app/src/app/appliances/[slug]/page.tsx
@@ -2,20 +2,18 @@ import {IProduct} from "src/types/product";
 import {Button} from "ui-components";
 
 export default async function Page({params}: { params: { slug: string } }) {
-    const productResponse = await fetch(`https://twworkspace--vtexsgdemostore.myvtex.com/_v/skus/2`);
-    const product: IProduct = await productResponse.json();
-    const productSkus = product.skus;
+    const productAndSKUIdResponse = await fetch(`https://twworkspace--vtexsgdemostore.myvtex.com/_v/skus/4`);
+    const productList: IProduct[] = await productAndSKUIdResponse.json();
 
     return (
         <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-            {productSkus.map(productSku => (
-                <div key={productSku.sku} style={{flex: '1', alignSelf: 'flex-start'}}>
-                    <img src={productSku.image}
-                         alt={productSku.skuname} width="100" height="100"/>
-                    <div style={{marginBottom: '20px'}}>
-                        {productSku.skuname}
+            {productList.map(product => (
+                <div key={product.productId} style={{flex: '1', alignSelf: 'flex-start'}}>
+                    <img src={product.skus[0].image}
+                         alt={product.name} width="100" height="100"/>
+                    <div>
+                        {product.name}
                     </div>
-
                     <Button label={'add to cart'} size={'large'}></Button>
                 </div>
             ))}
