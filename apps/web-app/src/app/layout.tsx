3f1cc980641497f39790f1163fcfa026db4a118f
wen <wen.xiao@thoughtworks.com>
2023-06-30 17:43:55 +0800

    set line height

--- apps/web-app/src/app/layout.tsx
+++ apps/web-app/src/app/layout.tsx
@@ -1,21 +1,22 @@
-import { CatalogsContextProvider } from "src/catalogs.context";
-import { Head } from "src/components/Head";
+import {CatalogsContextProvider} from "src/catalogs.context";
+import {Head} from "src/components/Head";
+import "../../src/styles/globals.css";
 
-export default function Root({ children }: { children: React.ReactNode }) {
-  return (
-    <html>
-      <body>
+export default function Root({children}: { children: React.ReactNode }) {
+    return (
+        <html>
+        <body>
         <CatalogsContextProvider>
-          <Head />
-          <div
-            style={{
-              paddingTop: "69px",
-            }}
-          >
-            {children}
-          </div>
+            <Head/>
+            <div
+                style={{
+                    paddingTop: "69px",
+                }}
+            >
+                {children}
+            </div>
         </CatalogsContextProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
