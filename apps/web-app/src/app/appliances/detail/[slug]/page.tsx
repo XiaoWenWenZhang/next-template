cfefaf3427d5b2c8ac3eea6b6cabbe7a19843416
lan.jiang <lan.jiang@thoughtworks.com>
2023-07-06 16:13:59 +0800

    add product detail page with sku detail

--- apps/web-app/src/app/appliances/detail/[slug]/page.tsx
+++ apps/web-app/src/app/appliances/detail/[slug]/page.tsx
@@ -1,10 +1,15 @@
-import Link from "next/link";
+'use client'
 import Image from "next/image";
 import ProductQuantitySelector from "src/components/ProductQuantitySelector";
 import {ICartProductSkus} from "src/types/cart";
 import {AddToCart} from "src/components/AddToCart";
+import {useRouter, useSearchParams} from "next/navigation";
 
 export default async function Page({params}: { params: { slug: string } }) {
+    const router = useRouter();
+    const searchParams = useSearchParams()
+    const skuId = searchParams.get('skuId')
+
     const productDetail = await fetch(`https://twworkspace--vtexsgdemostore.myvtex.com/_v/products/${params.slug}`)
         .then((res) => res.json())
         .then((response) => {
@@ -22,54 +27,58 @@ export default async function Page({params}: { params: { slug: string } }) {
         name: '',
     };
 
+    const currentProductSkus = productDetail.skus.find(sku => sku.sku == skuId) ?? productDetail.skus[0];
+
     return (
         <div style={{
             display: 'flex', flexDirection: 'row', width: '90%', padding: '0 100px'
         }}>
             <div style={{width: '50%'}}>
                 <img
-                    src={productDetail.skus[0].image}
+                    src={currentProductSkus.image}
                     alt={productDetail.name}
                     width="100%"
                     height="100%"
                 />
             </div>
             <div style={{width: '50%', padding: '100px 100px 50px 100px'}}>
-                <div style={{paddingBottom: '15px', fontWeight: 700, fontSize: '30px'}}>{productDetail.name}</div>
+                <div style={{paddingBottom: '15px', fontWeight: 700, fontSize: '30px'}}>{productDetail.name}{currentProductSkus.sku}</div>
 
                 <div style={{display: 'flex', flexDirection: 'row', marginTop: '20px'}}>
                     <div style={{
                         paddingBottom: '15px', fontWeight: 700, fontSize: '36px', width: '100%',
                         borderBottom: '1px solid #ccc'
-                    }}>{productDetail.skus[0].bestPriceFormated}</div>
+                    }}>{currentProductSkus.bestPriceFormated}</div>
                 </div>
                 <div style={{
                     display: 'flex',
                     justifyContent: 'start',
                     color: '#727273',
                     marginTop: '10px'
-                }}>Color: {productDetail.skus[0].dimensions.Color}
+                }}>Color: {currentProductSkus.dimensions.Color}
                 </div>
                 <div style={{
                     display: 'flex', flexDirection: 'row',
                 }}>
-                    <div
-                        style={{
-                            marginLeft: '5px',
-                            width: '42px',
-                            height: '42px',
-                            border: '1px solid red',
-                            borderRadius: '5px',
-                            margin: '25px, 10px,5px'
-                        }}>
-                        <Image
-                            style={{borderRadius: '5px', border: '1px solid #e3e4e6',}}
-                            src={productDetail.skus[0].image}
-                            alt={productDetail.name}
-                            width="40"
-                            height="40"
-                        />
-                    </div>
+                    {
+                        productDetail.skus.map(item =>(
+                            <div
+                                key={item.sku}
+                                onClick={() => {
+                                    console.log(item.sku)
+                                    router.push(`/appliances/detail/${productDetail.productId}?skuId=${item.sku}`);
+                                }}
+                                className={`product-review ${currentProductSkus.sku == item.sku ? 'active-product' : ''} `}>
+                                <Image
+                                    style={{borderRadius: '5px', border: '1px solid #e3e4e6',}}
+                                    src={item.image}
+                                    alt={productDetail.name}
+                                    width="40"
+                                    height="40"
+                                />
+                            </div>
+                        ))
+                    }
                 </div>
                 <div style={{
                     display: 'flex',
